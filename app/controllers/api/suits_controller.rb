db8e4d50ac337c3237f74c7cfed5529f3a338b42
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-01 11:57:39 -0500

    adds show atcion

--- app/controllers/api/suits_controller.rb
+++ app/controllers/api/suits_controller.rb
@@ -3,4 +3,10 @@ class Api::SuitsController < ApplicationController
     @suits = Suit.all
     render "index.json.jb"
   end
+
+  def show
+    id = params["id"]
+    @suit = Suit.find_by(id: id)
+    render "show.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # end
   namespace :api do
     get "/suits" => "suits#index"
+    get "/suits/:id" => "suits#show"
   end
 end
