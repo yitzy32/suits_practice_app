6f36c694ec9c50880bfe87a107283d1283008411
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-01 11:48:04 -0500

    adds index action

--- app/controllers/api/suits_controller.rb
+++ app/controllers/api/suits_controller.rb
@@ -1,2 +1,6 @@
 class Api::SuitsController < ApplicationController
+  def index
+    @suits = Suit.all
+    render "index.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    get "/suits" => "suits#index"
+  end
 end
